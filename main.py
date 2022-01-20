22f1b58ac1481b4f42076cb2e232656d9ee57d43
Enzo Kozoroski Giovanini <enzogiovanini@gmail.com>
2022-01-20 11:19:36 -0400

    Classe de formulario com validação de usuario

--- main.py
+++ main.py
@@ -1,28 +1,65 @@
 from tkinter import *
 
+from numpy import ma
+
 # classe para configuração de janela
 class Application:
     def __init__(self, master=None):
-        # Frame/container
-        self.widget1 = Frame(master)
-        self.widget1.pack()
-        # Mensagem do container
-        self.msg = Label(self.widget1, text="Primeiro widget")
-        self.msg["font"] = ("Verdana", "10", "italic", "bold")
-        self.msg.pack()
-        # Botao
-        self.btn = Button(self.widget1)
-        self.btn["text"] = "Sair"
-        self.btn["font"] = ("Calibri", "10")
-        self.btn["width"] = 10
-        self.btn.bind("<Button-1>", self.mudarTexto)
-        self.btn.pack()
+        self.fontePadrao = ("Arial", "10")
+        self.primeiroContainer = Frame(master)
+        self.primeiroContainer["pady"] = 10
+        self.primeiroContainer.pack()
 
-    def mudarTexto(self, event):
-        if self.msg["text"] == "Primeiro widget":
-            self.msg["text"] = "O botao recebeu um clique"
+        self.segundoContainer = Frame(master)
+        self.segundoContainer["padx"] = 20
+        self.segundoContainer.pack()
+
+        self.terceiroContainer = Frame(master)
+        self.terceiroContainer["padx"] = 20
+        self.terceiroContainer.pack()
+
+        self.quartoContainer = Frame(master)
+        self.quartoContainer["pady"] = 20
+        self.quartoContainer.pack()
+
+        self.titulo = Label(self.primeiroContainer, text="Dados do usuário")
+        self.titulo["font"] = ("Arial", "10", "bold")
+        self.titulo.pack()
+
+        self.nomeLabel = Label(self.segundoContainer, text="Nome", font=self.fontePadrao)
+        self.nomeLabel.pack(side=LEFT)
+
+        self.nome = Entry(self.segundoContainer)
+        self.nome["width"] = 30
+        self.nome["font"] = self.fontePadrao
+        self.nome.pack(side=LEFT)
+
+        self.senhaLabel = Label(self.terceiroContainer, text="Senha", font=self.fontePadrao)
+        self.senhaLabel.pack(side=LEFT)
+
+        self.senha = Entry(self.terceiroContainer)
+        self.senha["width"] = 30
+        self.senha["font"] = self.fontePadrao
+        self.senha["show"] = "*"
+        self.senha.pack(side=LEFT)
+
+        self.autenticar = Button(self.quartoContainer)
+        self.autenticar["text"] = "Autenticar"
+        self.autenticar["font"] = ("Calibri", "8")
+        self.autenticar["width"] = 12
+        self.autenticar["command"] = self.verificaSenha
+        self.autenticar.pack()
+
+        self.mensagem = Label(self.quartoContainer, text="", font=self.fontePadrao)
+        self.mensagem.pack()
+
+    def verificaSenha(self):
+        usuario = self.nome.get()
+        senha = self.senha.get()
+        if usuario == "usuario" and senha == "dev":
+            self.mensagem["text"] = "Autenticado"
         else:
-            self.msg["text"] = "Primeiro widget"
+            self.mensagem["text"] = "Erro na autenticação"    
 
 root = Tk()
 Application(root)
